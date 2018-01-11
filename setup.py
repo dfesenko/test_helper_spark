593f13fd736b010777b245d25c31aef91b91d313
dfesenko <34854440+dfesenko@users.noreply.github.com>
2018-01-11 16:52:00 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'test_helper_spark',
+  packages = ['test_helper_spark'],
+  version = '0.1',
+  description = 'A testing helper for "Intro to Big Data with Apache Spark" course',
+  author = 'Dmytro Fesenko, Volodymyr Rospotniuk',
+  author_email = 'fesenkogithub@gmail.com, rospotniuk@gmail.com',
+  url = 'https://github.com/rospotniuk/test_helper',
+  keywords = ['testing', 'autograder', 'spark'],
+  classifiers = [],
+  include_package_data = True
+)
